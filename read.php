b8fcc4445ad7ca4a8c906a629522fc6977e55e0c
hnlylmlzh <50590064@qq.com>
2017-01-12 10:49:36 +0800

    test

--- read.php
+++ read.php
@@ -71,9 +71,17 @@ tail -n 40 /var/log/messages
 
 tail -n 40 /var/log/syslog
 tail -n 40 /var/log/auth.log
+
+tail -n 40 /var/log/messages 
  
 composer /home/comp/silk 
 
 Cannot create cache directory /home/comp/.composer/cache/repo/https---packagist.phpcomposer.com/, or directory is not writable. Proceeding without cache
 Cannot create cache directory /home/comp/.composer/cache/files/, or directory is not writable. Proceeding without cache
 
+
+Jan 12 09:31:32 iZ25isatkrvZ kernel: [2249136.146279] [28392]  1005 28392    50762    13866   0       0             0 php
+Jan 12 09:31:32 iZ25isatkrvZ kernel: [2249136.146281] Out of memory: Kill process 28392 (php) score 111 or sacrifice child
+Jan 12 09:31:32 iZ25isatkrvZ kernel: [2249136.148078] Killed process 28392 (php) total-vm:203048kB, anon-rss:55464kB, file-rss:0kB
+
+test222;
